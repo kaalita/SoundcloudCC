a8ec2920e7ff8eb90884949c18fc382db863abee
Katrin Apel <katrin.apel@gmail.com>
2012-12-11 00:17:57 +0100

    Initial Commit

---
+++ SoundcloudCC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoundcloudCC
+//
+//  Created by Katrin on 12/11/12.
+//  Copyright (c) 2012 Katrin Apel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
